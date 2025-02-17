39aea6f7d219586915b1906b76aa4bd461293aac
ibali <i.balijon@gmail.com>
2025-02-17 19:28:16 +0100

    fix import

--- js/main.js
+++ js/main.js
@@ -1,6 +1,5 @@
 import { Engine } from "./engine.js";
 import { Input } from "./input.js";
-import { gameState } from "./gameState.js";
 
 document.addEventListener("DOMContentLoaded", () => {
     const input = new Input();
